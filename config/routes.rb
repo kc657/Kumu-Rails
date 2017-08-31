ef341726b5526379816bc796527ce85d2e6c04de
Ghizmilth <hidanerferrer@gmail.com>
2017-08-30 21:56:43 -0700

    Created a view for Sign-Up section

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
-  root "classrooms#index"
+  #root "classrooms#index"
+  root to: "users#index"
 
-  get "/classrooms", to: "classrooms#index", as: "classrooms"
+#clasrooms routes
+#  get "/classrooms", to: "classrooms#index", as: "classrooms"
 
+#new user signup
+  get '/users/new', to: 'users#new', as: 'new_user'
+  post '/users', to: 'users#create'
 end
