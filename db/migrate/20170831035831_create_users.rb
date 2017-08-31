42f72a37af4f1c8d24ef84c883c2cdd23e761d06
ryan-barrettt <ryan.barrettt@spookyhouse.org>
2017-08-31 11:25:22 -0700

    user model conflict resolve

--- db/migrate/20170831035831_create_users.rb
+++ db/migrate/20170831035831_create_users.rb
@@ -4,7 +4,7 @@ class CreateUsers < ActiveRecord::Migration[5.1]
       t.string :first_name
       t.string :last_name
       t.string :username
-      t.integer :password
+      t.string :password_digest
 
       t.timestamps
     end
