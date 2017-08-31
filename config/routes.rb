685858e19f65001a2716b6e2a01d3b206c376bfe
ryan-barrettt <ryan.barrettt@spookyhouse.org>
2017-08-31 11:04:42 -0700

    "began early framework for user profile page"

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root "classrooms#index"
 
   get "/classrooms", to: "classrooms#index", as: "classrooms"
-
+  get "/users", to: "users#index", as: "users"
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170831000632) do
+ActiveRecord::Schema.define(version: 20170831035831) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,6 +18,15 @@ ActiveRecord::Schema.define(version: 20170831000632) do
   create_table "classrooms", force: :cascade do |t|
     t.string "name"
     t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "username"
+    t.integer "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
