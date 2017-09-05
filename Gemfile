62622ab5835e670f6cb48a6aeaeaa4ff9c9ed216
ryan-barrett <ryan.barrett@spookyhouse.org>
2017-09-05 12:19:04 -0700

    Added the option to upload an image representing the user on user profile page

--- Gemfile
+++ Gemfile
@@ -39,6 +39,7 @@ gem 'jbuilder', '~> 2.5'
 gem 'bcrypt', '~> 3.1.7'
 #hirb
 gem 'hirb'
+gem 'paperclip', '~> 5.0.0'
 
 # Use Capistrano for deployment
 # gem 'capistrano-rails', group: :development
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  has_attached_file :image, styles: { large: "600x600>", medium: "400x400>", thumb: "100x100#"}
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\Z/
+
   def self.confirm(params)
     @user = User.find_by({username: params[:username]})
     @user ? @user.authenticate(params[:password]) : false
