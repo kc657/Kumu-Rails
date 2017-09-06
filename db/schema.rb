87cbe316c98b20ef62457868bcc5539274aa5dae
Ghizmilth <hidanerferrer@gmail.com>
2017-09-06 10:52:50 -0700

    Complete associations models. 1:1 and 1:M connections added and tested

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905174135) do
+ActiveRecord::Schema.define(version: 20170906035605) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,13 +24,31 @@ ActiveRecord::Schema.define(version: 20170905174135) do
 
   create_table "posts", force: :cascade do |t|
     t.bigint "user_id"
+    t.bigint "topic_id"
     t.string "name"
     t.string "description"
     t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string "content"
+    t.index ["topic_id"], name: "index_posts_on_topic_id"
     t.index ["user_id"], name: "index_posts_on_user_id"
+  end
+
+  create_table "topics", force: :cascade do |t|
+    t.string "name"
+    t.string "description"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "user_topics", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "topic_id"
+    t.index ["topic_id"], name: "index_user_topics_on_topic_id"
+    t.index ["user_id"], name: "index_user_topics_on_user_id"
   end
 
   create_table "users", force: :cascade do |t|
@@ -63,4 +81,6 @@ ActiveRecord::Schema.define(version: 20170905174135) do
     t.index ["voter_type", "voter_id"], name: "index_votes_on_voter_type_and_voter_id"
   end
 
+  add_foreign_key "user_topics", "topics"
+  add_foreign_key "user_topics", "users"
 end
