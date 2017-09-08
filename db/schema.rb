b512dbac8faa5138171cb5eb257045fdd51d4171
Justin Castilla <pxlperfection@gmail.com>
2017-09-08 10:59:11 -0700

    added suggestions

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170906192114) do
+ActiveRecord::Schema.define(version: 20170908162923) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -40,13 +40,9 @@ ActiveRecord::Schema.define(version: 20170906192114) do
   create_table "topics", force: :cascade do |t|
     t.string "name"
     t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "user_topics", force: :cascade do |t|
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "topic_id"
     t.index ["topic_id"], name: "index_user_topics_on_topic_id"
